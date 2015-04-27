5a646c1fd46783d9875b9468e1bf311ecde50528
Keegan <kmp3325@rit.edu>
2015-04-27 19:32:16 -0400

    Add junit to pom.

---
+++ src/test/logic/RemindersHandlerTest.java
@@ -0,0 +1,14 @@
+package logic;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by keegan on 4/27/15.
+ */
+public class RemindersHandlerTest extends TestCase {
+
+    public void testParseTime() {
+
+    }
+}
